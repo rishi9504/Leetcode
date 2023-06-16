bfe9a9f859b7203d79926f83fadc45583ec68d60
rishi9504 <hrishikesh.pandey9955@gmail.com>
2023-06-16 14:08:19 +0000

    [LeetCode Sync] Runtime - 48 ms (5.55%), Memory - 16.3 MB (99.94%)

--- 0058-length-of-last-word/solution.py
+++ 0058-length-of-last-word/solution.py
@@ -1,6 +1,5 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        x = s.split()
-        y=len(x[len(x)-1])
-        return y
-        
+        s_split = s.split()
+        *a,b = s_split
+        return len(b)
