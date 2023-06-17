99ef9180f3ab774af337e01d4eeee80ce756fcf5
rishi9504 <hrishikesh.pandey9955@gmail.com>
2023-06-17 08:35:06 +0000

    [LeetCode Sync] Runtime - 59 ms (33.69%), Memory - 16.1 MB (99.97%)

--- 0009-palindrome-number/solution.py
+++ 0009-palindrome-number/solution.py
@@ -1,16 +1,4 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
-        s= str(x)
-        left,right=0,len(s)-1
-        
-        while left<right:
-            if s[left] !=s[right]:
-                return False
-            left+=1
-            right-=1
-        return True    
-        
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        data = str(x)
+        return data == data[::-1]
