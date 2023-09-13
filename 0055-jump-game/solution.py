9116bf320f72a855d2c66c986537d072186d6061
rishi9504 <hrishikesh.pandey9955@gmail.com>
2023-09-13 10:22:47 +0000

    [LeetCode Sync] Runtime - 426 ms (20.65%), Memory - 17.4 MB (99.50%)

---
+++ 0055-jump-game/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        reachable = 0
+        for i in range(len(nums)):
+            if reachable < i :
+                return False
+            reachable = max(reachable, i+nums[i])
+        return True
