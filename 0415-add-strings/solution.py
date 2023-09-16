9c57ddbd3b0e732394bbae7172e50c2e4e037852
rishi9504 <hrishikesh.pandey9955@gmail.com>
2023-09-16 17:20:19 +0000

    [LeetCode Sync] Runtime - 76 ms (5.20%), Memory - 16.7 MB (63.45%)

--- 0415-add-strings/solution.py
+++ 0415-add-strings/solution.py
@@ -1,4 +1,28 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        return str(int(num1)+int(num2))
+        num1, num2 = num1[::-1], num2[::-1]
+
+        length_diff = len(num1) - len(num2)
+
+        if length_diff < 0:
+
+            num1 += "0" * -length_diff
+
+        else:
+
+            num2 += "0" * length_diff
+
+        result, carry = [], 0
+
+        for d1, d2 in zip(num1, num2):
+
+            carry, digit = divmod(int(d1) + int(d2) + carry, 10)
+
+            result.append(str(digit))
+
+        if carry:
+
+            result.append(str(carry))
+
+        return "".join(result[::-1])
         
