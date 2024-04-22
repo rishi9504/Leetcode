3652a1dd7755d1be36716ba9468937e4b4b1b339
rishi9504 <hrishikesh.pandey9955@gmail.com>
2024-04-22 10:11:35 +0000

    [LeetCode Sync] Runtime - 50 ms (94.15%), Memory - 17.7 MB (59.77%)

--- 0001-two-sum/solution.py
+++ 0001-two-sum/solution.py
@@ -1,18 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-         # Create a dictionary to store the complement of each number
-        complement_map = {}
-    
-    # Iterate through the array
+        dict_done={}
         for i, num in enumerate(nums):
-        # Check if the complement of the current number exists in the dictionary
-            complement = target - num
-            if complement in complement_map:
-            # Return the indices of the two numbers
-                return [complement_map[complement], i]
+            if target-num in dict_done:
+                return [dict_done[target-num],i]
+            dict_done[num] = i
+        return []        
+
         
-        # Store the index of the current number in the dictionary
-            complement_map[num] = i
-    
-    # If no solution is found, return an empty list
-        return []
