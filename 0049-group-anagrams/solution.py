2548a5ffe6c5e4595f1295cc6aa02f22f3f9f999
rishi9504 <hrishikesh.pandey9955@gmail.com>
2024-04-22 10:36:28 +0000

    [LeetCode Sync] Runtime - 85 ms (77.48%), Memory - 19.6 MB (86.53%)

--- 0049-group-anagrams/solution.py
+++ 0049-group-anagrams/solution.py
@@ -1,19 +1,9 @@
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-         # Create a dictionary to store the groups of anagrams
-        anagram_groups = {}
-    
-    # Iterate through each string in the array
-        for s in strs:
-        # Sort the string to use it as a key in the dictionary
-            sorted_s = ''.join(sorted(s))
+    def groupAnagrams(self, strs):
+        anagram_map = defaultdict(list)
         
-        # If the sorted string is not in the dictionary, add it with an empty list as the value
-            if sorted_s not in anagram_groups:
-                anagram_groups[sorted_s] = []
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            anagram_map[sorted_word].append(word)
         
-        # Add the original string to the list of anagrams
-            anagram_groups[sorted_s].append(s)
-    
-    # Return the values from the dictionary as the grouped anagrams
-        return list(anagram_groups.values())
+        return list(anagram_map.values())
