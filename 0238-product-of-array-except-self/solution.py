52c66e52b2ef6609c44c7d5f59750de080c3c751
rishi9504 <hrishikesh.pandey9955@gmail.com>
2024-04-25 06:32:49 +0000

    [LeetCode Sync] Runtime - 273 ms (48.41%), Memory - 25.7 MB (57.84%)

--- 0238-product-of-array-except-self/solution.py
+++ 0238-product-of-array-except-self/solution.py
@@ -8,10 +8,13 @@ class Solution:
         sums = [1]
         for i in range(1,len(nums)):
             sums.append(nums[i-1]*sums[-1])
+        print(sums)
 
         final=1
         for i in range(len(nums)-1,-1,-1):
             sums[i]*=final
             final*=nums[i]
-        return sums        
+        return sums   
+        
+             
         
