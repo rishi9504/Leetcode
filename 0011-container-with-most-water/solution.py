bd6c85e8459f3388377479465f883f12bafce7af
rishi9504 <hrishikesh.pandey9955@gmail.com>
2024-04-25 13:30:35 +0000

    [LeetCode Sync] Runtime - 515 ms (76.31%), Memory - 30.2 MB (9.08%)

--- 0011-container-with-most-water/solution.py
+++ 0011-container-with-most-water/solution.py
@@ -2,13 +2,15 @@ class Solution:
     def maxArea(self, height: List[int]) -> int:
         left = 0
         right = len(height)-1
+        #started with widest seperation of the lines
         max_area = (right-left) * min(height[right],height[left])
 
-        while left < right :
-            if height[left] < height[right]:
+        while left < right : #any lesser sep should have more area than prev calc.
+            if height[left] < height[right]: 
                 left+=1
             else:
                 right-=1
+            #calculate max area and newly calculated area    
             max_area = max(max_area,(right-left)*min(height[right],height[left]))
         return max_area            
         
