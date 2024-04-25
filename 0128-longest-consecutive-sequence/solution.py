a9621affa097a870c0de20221441be8d21c6b516
rishi9504 <hrishikesh.pandey9955@gmail.com>
2024-04-25 09:48:20 +0000

    [LeetCode Sync] Runtime - 354 ms (62.06%), Memory - 31.9 MB (45.88%)

--- 0128-longest-consecutive-sequence/solution.py
+++ 0128-longest-consecutive-sequence/solution.py
@@ -1,26 +1,23 @@
+from collections import defaultdict
 class Solution:
-    def longestConsecutive(self, nums: List[int]) -> int:
-         # Create a set to store the unique elements of the array
-        num_set = set(nums)
-    
-    # Initialize the maximum length
-        max_length = 0
-    
-    # Iterate through the array
-        for num in nums:
-        # Check if the current element is the start of a sequence
-            if num - 1 not in num_set:
-            # Calculate the length of the consecutive sequence
-                current_length = 1
-                current_num = num
-            
-            # Increment the current element until the next element is not in the set
-                while current_num + 1 in num_set:
-                    current_length += 1
-                    current_num += 1
-            
-            # Update the maximum length if necessary
-                max_length = max(max_length, current_length)
-    
-    # Return the maximum length of the consecutive sequence
-        return max_length
+    def longestConsecutive(self, nums: List[int]) -> int:        
+        # m = defaultdict(list)
+        # for i,n in enumerate(nums):
+        #     m[n].append(n)
+        #     if n-1 in m:
+        m = set(nums)
+        longest = 0
+
+        for i in m:
+            if i-1 in m:
+                continue
+            seq=0
+            while i in m:
+                seq+=1
+                i+=1
+            longest = max(longest,seq)
+        return longest            
+
+
+
+
