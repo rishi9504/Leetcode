1ac019d27246bf9372da9fd10db863455544a6f5
rishi9504 <hrishikesh.pandey9955@gmail.com>
2024-04-29 10:23:31 +0000

    [LeetCode Sync] Runtime - 41 ms (77.47%), Memory - 17.3 MB (35.56%)

--- 0125-valid-palindrome/solution.py
+++ 0125-valid-palindrome/solution.py
@@ -1,8 +1,13 @@
+
+import string
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        if s == " ":
-            return True
-        else:
-            filter = ''.join([i.lower() for i in s if i.isalnum()])
-            return filter == filter[::-1]
-
+        chars = set(string.ascii_lowercase+string.digits)
+        s = [c for c in s.lower() if c   in chars ]
+        l,r = 0,len(s)-1
+        while l<r:
+            if s[l].lower()!=s[r].lower():
+                return False
+            l+=1
+            r-=1
+        return True
