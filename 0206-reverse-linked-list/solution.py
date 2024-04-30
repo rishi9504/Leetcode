72bf63d88f32f41bc80aa2c5127cbeed4d897058
rishi9504 <hrishikesh.pandey9955@gmail.com>
2024-04-30 12:42:36 +0000

    [LeetCode Sync] Runtime - 39 ms (43.39%), Memory - 17.6 MB (89.24%)

--- 0206-reverse-linked-list/solution.py
+++ 0206-reverse-linked-list/solution.py
@@ -5,8 +5,12 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        #setting a prev node
         prev = None
+        #setting current node to head
         current = head
+        # loop for finding the last element, once found set the next to last node
+        # now move back linking the elements backwards using the pointer
         while(current is not None):
             next = current.next
             current.next = prev
