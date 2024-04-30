ebabb87decd327217e9e9c5d423a10d3afcf5e8c
rishi9504 <hrishikesh.pandey9955@gmail.com>
2024-04-30 13:04:28 +0000

    [LeetCode Sync] Runtime - 99 ms (95.74%), Memory - 18.9 MB (95.75%)

--- 0136-single-number/solution.py
+++ 0136-single-number/solution.py
@@ -1,7 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        xor = 0
+        xor=0
         for num in nums:
-            xor ^=num
+            xor^=num
         return xor    
         
