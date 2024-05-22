cf57cd5692cfd8e097b7c00d84b239f2476cb513
rishi9504 <hrishikesh.pandey9955@gmail.com>
2024-05-22 10:11:38 +0000

    [LeetCode Sync] Runtime - 42 ms (27.60%), Memory - 16.5 MB (45.12%)

--- 0021-merge-two-sorted-lists/solution.py
+++ 0021-merge-two-sorted-lists/solution.py
@@ -1,24 +1,36 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def mergeTwoLists(self, l1, l2):
-        """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        prev = dummy = ListNode(None)
+class Solution:
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        # out =[]
+        # i,j = 0,0
+        # while i<len(list1) and j<len(list2):
+        #     if list1[i]<list2[j]:
+        #         out.append(list1[i])
+        #         i+=1
+        #     else:
+        #         out.append(list2[j])
+        #         j+=1
+        # while i<len(list1):
+        #     out.append(list1[i])
+        #     i+=1
+        # while j<len(list2):
+        #     out.append(list2[j])
+        #     j+=1
+        p = d = ListNode(None)
         while l1 and l2:
             if l1.val < l2.val:
-                prev.next = l1
+                p.next = l1
                 l1 = l1.next
             else:
-                prev.next = l2
+                p.next = l2
                 l2 = l2.next
-            prev = prev.next
-        prev.next = l1 or l2
-        return dummy.next
+            p = p.next
+        p.next = l1 or l2
+        return d.next        
+
+
         
