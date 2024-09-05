ef998ae93b5adb9f6020813e4d1204cd5d5df589
rishi9504 <hrishikesh.pandey9955@gmail.com>
2024-09-05 06:51:53 +0000

    [LeetCode Sync] Runtime - 36 ms (66.27%), Memory - 16.5 MB (75.14%)

--- 0075-sort-colors/solution.py
+++ 0075-sort-colors/solution.py
@@ -3,41 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # nums = nums.sort()
 
-        z,o,t = 0,0,0
+        z,o = 0,0
         for n in nums:
             if n==0:
                 z+=1
             elif n==1:
                 o+=1
-            else:
-                t+=1
         for i in range(0,z):
             nums[i]=0
         for i in range(z,z+o):
             nums[i]=1
         for i in range(z+o,len(nums)):
             nums[i]=2                
-        # print(f'{z=},{o=}{t=}')
-        # c = 0
-        # i=0
-        # for i in range(z):
-        #     nums[i+c]=0
-        #     print(f'{i=},{nums=}')
-        # print(nums)
-        # c=i+1
-        # print(f'{c=}')
-        # for i in range(o):
-        #     nums[i+c]=1
-        #     print(f'{i=},{nums=}')
-        # c=c+i+1
-        # print(f'{c=}')
-        # print(nums)
-        # for i in range(t):
-        #     nums[i+c]=2
-        #     print(f'{i=},{nums=}')
-        # print(nums)
-        
-        
-        # return nums
+
