e36b140e4caa0869c4a675d9d8486147d30034af
rishi9504 <hrishikesh.pandey9955@gmail.com>
2024-09-17 17:09:01 +0000

    [LeetCode Sync] Runtime - 36 ms (53.18%), Memory - 16.7 MB (16.50%)

---
+++ 0904-leaf-similar-trees/solution.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        # dfs mentioned in blind 75
+
+        def dfs(node):
+            if not node:
+                return []
+            if not node.left and not node.right:
+                return [node.val]
+            return dfs(node.left) + dfs(node.right)
+        return dfs(root1) == dfs(root2)            
+        
