ec7096ac61028c3f677e58168ff1ac2d9b6e434c
rishi9504 <hrishikesh.pandey9955@gmail.com>
2024-09-17 04:44:42 +0000

    [LeetCode Sync] Runtime - 123 ms (80.95%), Memory - 31.2 MB (16.22%)

---
+++ 1544-count-good-nodes-in-binary-tree/solution.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        def dfs(node,maxValue):
+            if node is None:
+                return 0
+            res = 1 if node.val>=maxValue else 0
+            maxValue = max(maxValue,node.val)
+            res+=dfs(node.left,maxValue)
+            res+=dfs(node.right,maxValue)
+            return res
+        return dfs(root,root.val)    
+
+        
