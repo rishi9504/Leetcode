c93d0ee75ade14d3d5411a3ad3ebf495ce72d047
rishi9504 <hrishikesh.pandey9955@gmail.com>
2024-10-28 10:00:58 +0000

    [LeetCode Sync] Runtime - 28 ms (92.61%), Memory - 16.5 MB (56.59%)

--- 3617-find-the-original-typed-string-i/solution.py
+++ 3617-find-the-original-typed-string-i/solution.py
@@ -1,10 +1,12 @@
 class Solution:
     def possibleStringCount(self, word: str) -> int:
-        po = []
-        po.append(word)
+        po = 1
+        # po.append(word)
         for i in range(1,len(word)):
             if word[i]==word[i-1]:
-                pw = word[:i]+word[i+1:]
-                po.append(pw)
-        return len(po)    
+                # pw = word[:i]+word[i+1:]
+                # po.append(pw)
+                # print (po)
+                po+=1
+        return po   
         
