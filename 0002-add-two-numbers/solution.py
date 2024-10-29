9820e1abe4518ba766d640a8ae604fc3832c7432
rishi9504 <hrishikesh.pandey9955@gmail.com>
2024-10-29 07:21:29 +0000

    [LeetCode Sync] Runtime - 11 ms (9.34%), Memory - 16.7 MB (14.59%)

--- 0002-add-two-numbers/solution.py
+++ 0002-add-two-numbers/solution.py
@@ -1,59 +1,23 @@
-# # Definition for singly-linked list.
-# # class ListNode:
-# #     def __init__(self, val=0, next=None):
-# #         self.val = val
-# #         self.next = next
-# class Solution:
-#     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-#         cur = ListNode()
-#         head = cur
-#         c = 0
-#         while l1 is not None or l2 is not None or c != 0:
-#             n1 = l1.val if l1 is not None else 0
-#             n2 = l2.val if l2 is not None else 0
-#             s = n1+n2+c
-#             print(n1,n2,c,s)
-#             n = s%10
-#             c = s//10
-#             # c,n = divmod(s,10)
-#             # if l1.next is None or l2.next is None:
-#             #     break
-#             _next = ListNode(n)
-#             head.next = _next
-#             cur = head.next
-#             l1=l1.next if l1 is not None else None
-#             l2=l2.next if l2 is not None else None
-#         # while l1 is not None or l2 is not None:
-#         # res = cur.next
-#         res = cur.next
-#         cur.next = None
-
-
-
-#         return res
-
-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+    def addTwoNumbers(self, head1: Optional[ListNode], head2: Optional[ListNode]) -> Optional[ListNode]:
         dummyHead = ListNode(0)
-        tail = dummyHead
+        current = dummyHead
         carry = 0
-
-        while l1 is not None or l2 is not None or carry != 0:
-            digit1 = l1.val if l1 is not None else 0
-            digit2 = l2.val if l2 is not None else 0
-
-            sum = digit1 + digit2 + carry
-            digit = sum % 10
-            carry = sum // 10
-
-            newNode = ListNode(digit)
-            tail.next = newNode
-            tail = tail.next
-
-            l1 = l1.next if l1 is not None else None
-            l2 = l2.next if l2 is not None else None
-
-        result = dummyHead.next
-        dummyHead.next = None
-        return result
+        while head1 is not None or head2 is not None or carry != 0:
+            x = head1.val if head1 is not None else 0
+            y = head2.val if head2 is not None else 0
+            sum = carry + x + y
+            carry = 1 if sum > 9 else 0
+            current.next = ListNode(sum % 10)
+            current = current.next
+            if head1 is not None:
+                head1 = head1.next
+            if head2 is not None:
+                head2 = head2.next
+        return dummyHead.next
+            
