0eebecfe685f93d843f4ed388f6d2305a8ba8562
rishi9504 <hrishikesh.pandey9955@gmail.com>
2024-10-29 09:47:48 +0000

    [LeetCode Sync] Runtime - 24 ms (82.71%), Memory - 39.2 MB (7.48%)

--- 0234-palindrome-linked-list/solution.py
+++ 0234-palindrome-linked-list/solution.py
@@ -1,33 +1,13 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def isPalindrome(self, head):
-        """
-        :type head: ListNode
-        :rtype: bool
-        """
-        fast, slow = head, head
-        rev = None                      # head of the already-reversed part
-
-        while fast and fast.next:
-            fast = fast.next.next
-            next_slow = slow.next
-            slow.next = rev
-            rev = slow
-            slow = next_slow
-
-        # if fast is not null, slow is middle element of odd length list which is skipped
-        # if fast is null, slow is first element of 2nd half of even length list
-        if fast:
-            slow = slow.next
-
-        while slow:
-            if slow.val != rev.val:
-                return False
-            slow = slow.next
-            rev = rev.next
-
-        return True
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        l = []
+        while head:
+            l.append(head.val)
+            head=head.next
+        return l==l[::-1]
+        
