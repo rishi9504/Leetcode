233f6c09e5dfad04220ab874e0a9d40963326e02
rishi9504 <hrishikesh.pandey9955@gmail.com>
2025-01-06 11:52:32 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.7 MB (16.97%)

--- 0027-remove-element/solution.py
+++ 0027-remove-element/solution.py
@@ -1,4 +1,14 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        while val in nums:
-            nums.remove(val)
+        # while val in nums:
+        #     nums.remove(val)
+        #while that work, tried solving two pointer app
+
+        k=0
+        for i in range(len(nums)):
+            if nums[i]!=val:
+                nums[k]=nums[i]
+                k+=1
+        return k                   
+
+
