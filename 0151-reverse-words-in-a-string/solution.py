24c072d04ae5142426a3004554f27dbe592db8ee
rishi9504 <hrishikesh.pandey9955@gmail.com>
2025-01-06 13:22:56 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18 MB (14.28%)

--- 0151-reverse-words-in-a-string/solution.py
+++ 0151-reverse-words-in-a-string/solution.py
@@ -1,5 +1,15 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        s_str = s.split()
-        s_str.reverse()
-        return " ".join(s_str)
+        # s_str = s.split()
+        # s_str.reverse()
+        # return " ".join(s_str)
+        
+        # two pointer approach
+
+        w = s.split()
+        l,r = 0,len(w)-1
+        while l<r:
+            w[l],w[r] = w[r],w[l]
+            l+=1
+            r-=1
+        return ' '.join(w)            
