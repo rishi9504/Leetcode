465f8558c1bd6bc9c727aeb851657e8cfd22056a
rishi9504 <hrishikesh.pandey9955@gmail.com>
2025-01-06 13:18:20 +0000

    [LeetCode Sync] Runtime - 3 ms (36.44%), Memory - 18.1 MB (8.85%)

--- 0541-reverse-string-ii/solution.py
+++ 0541-reverse-string-ii/solution.py
@@ -1,7 +1,19 @@
 class Solution:
     def reverseStr(self, s: str, k: int) -> str:
-        s=list(s)
-        for i in range(0,len(s),2*k):
-            s[i:i+k]=(s[i:i+k])[::-1]
-        return ''.join(s)
+        #easy method
+        # s=list(s)
+        # for i in range(0,len(s),2*k):
+        #     s[i:i+k]=(s[i:i+k])[::-1]
+        # return ''.join(s)
+
+        s = list(s)
+        n = len(s)
+        for i in range(0,n,2*k):
+            l=i
+            r = min(i+k-1,n-1)
+            while l<r:
+                s[l],s[r] = s[r],s[l]
+                l+=1
+                r-=1
+        return ''.join(s)        
             
