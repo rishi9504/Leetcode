d838f25997e59b33759fddef434aa113dbda0d0e
rishi9504 <hrishikesh.pandey9955@gmail.com>
2025-01-07 15:08:57 +0000

    [LeetCode Sync] Runtime - 15 ms (36.77%), Memory - 19.1 MB (30.80%)

---
+++ 0658-find-k-closest-elements/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        l,r = 0,len(arr)-1
+        while r-l>=k:
+            if abs(arr[l]-x) > abs(arr[r]-x):
+                l+=1
+            else:
+                r-=1
+        return arr[l:r+1]            
+        
