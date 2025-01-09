09d2ea589e90f65a2d56f9d94e5750d797ce4e7f
rishi9504 <hrishikesh.pandey9955@gmail.com>
2025-01-09 10:20:41 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.7 MB (26.50%)

--- 0001-two-sum/solution.py
+++ 0001-two-sum/solution.py
@@ -1,10 +1,15 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dict_done={}
-        for i, num in enumerate(nums):
-            if target-num in dict_done:
-                return [dict_done[target-num],i]
-            dict_done[num] = i
-        return []        
+
+        lookup = {}
+        for i,n in enumerate(nums):
+            if target-n in lookup:
+                return [lookup[target-n],i]
+            lookup[n]=i
+        return []
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         if nums[i]+nums[j]==target:
+        #             return [i,j]
 
         
