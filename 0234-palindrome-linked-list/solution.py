2dbc5998d67abfe2b763b6ffcbcd884ba46cc50d
rishi9504 <hrishikesh.pandey9955@gmail.com>
2025-01-09 14:02:36 +0000

    [LeetCode Sync] Runtime - 41 ms (34.69%), Memory - 34.8 MB (64.61%)

--- 0234-palindrome-linked-list/solution.py
+++ 0234-palindrome-linked-list/solution.py
@@ -5,9 +5,29 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        l = []
-        while head:
-            l.append(head.val)
-            head=head.next
-        return l==l[::-1]
+        #step 1: find middle
+        if not head: return []
+        slow, fast = head, head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        #step 2: reverse second half
+        prev, curr = None, slow.next
+        while curr:
+            nextt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nextt    
+        slow.next = None
+        
+        #step 3: merge lists
+        head1, head2 = head, prev
+        while head2:
+            if head2.val != head1.val:
+                return False
+            head1=head1.next
+            head2=head2.next
+        return True        
+
         
