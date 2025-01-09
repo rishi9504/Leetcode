10c81c8153a61b0384f471f91cd667ce0b0b704b
rishi9504 <hrishikesh.pandey9955@gmail.com>
2025-01-09 10:43:03 +0000

    [LeetCode Sync] Runtime - 3 ms (9.03%), Memory - 17.9 MB (12.75%)

--- 0953-reverse-only-letters/solution.py
+++ 0953-reverse-only-letters/solution.py
@@ -1,22 +1,14 @@
 class Solution:
     def reverseOnlyLetters(self, S):
-        # S[::-1]
-        # r = [s for s in S if s.isalpha()]
-        # return "".join(S[i] if not S[i].isalpha() else r.pop() for i in range(len(S)))
-
-        #update, use two pointer approach, swap if isalpha or else move the l or r pointer
-        s = list(S)
-        l,r = 0,len(S)-1
+        l,r=0,len(S)-1
+        S=list(S)
         while l<r:
-            if not s[l].isalpha():
-                l+=1
-                continue
-            if not s[r].isalpha():
-                r-=1
-                continue
-            else:
-                s[l],s[r] = s[r],s[l] 
-                l+=1
-                r-=1
-        return ''.join(s)               
+            while l<r and not S[l].isalpha():
+                l+=1             
+            while l<r and not S[r].isalpha():
+                r-=1  
+            S[l],S[r]=S[r],S[l]           
+            l+=1             
+            r-=1    
+        return ''.join(S)         
 
