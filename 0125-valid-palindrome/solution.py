c9c08cfe400bc2fc56612d571d99bc8c70d6ec57
rishi9504 <hrishikesh.pandey9955@gmail.com>
2025-01-09 10:28:53 +0000

    [LeetCode Sync] Runtime - 7 ms (81.11%), Memory - 18 MB (51.98%)

--- 0125-valid-palindrome/solution.py
+++ 0125-valid-palindrome/solution.py
@@ -2,16 +2,17 @@
 import string
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # two pointer technique with string normalization
-        l,r = 0,len(s)-1
+        s=s.lower()
+        l,r=0,len(s)-1
+
         while l<r:
             while l<r and not s[l].isalnum():
                 l+=1
             while l<r and not s[r].isalnum():
                 r-=1
-            if s[l].lower()!= s[r].lower():
+            if s[l]!=s[r]:
                 return False
             l+=1
             r-=1
-        return True            
+        return True
 
